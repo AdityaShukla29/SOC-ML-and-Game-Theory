8dfc38d46fc0b4eb0549b13ffa684d0d819c02c7
AdityaShukla29 <aditshukla20@gmail.com>
2024-06-23 00:31:41 +0530

    ADD: Adding the solution to Q2 from Documentation

---
+++ Assignment_1/Part_1/Q2/q2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def PCA(init_array: pd.DataFrame):
+
+    sorted_eigenvalues = None
+    final_data = None
+    dimensions = 2
+
+    # TODO: transform init_array to final_data using PCA
+
+    
+    # END TODO
+
+    return sorted_eigenvalues, final_data
+
+
+if __name__ == '__main__':
+    init_array = pd.read_csv("pca_data.csv", header = None)
+    sorted_eigenvalues, final_data = PCA(init_array)
+    np.savetxt("transform.csv", final_data, delimiter = ',')
+    for eig in sorted_eigenvalues:
+        print(eig)
+
+    # TODO: plot and save a scatter plot of final_data to out.png
+
+first_column = final_data[:, 0]  
+second_column = final_data[:, 1]
+  
+plt.scatter(first_column,second_column)
+plt.xlim(-15, 15)
+plt.ylim(-15, 15)
+plt.show()
+    # END TODO
